fd39bbcc184fb5a82d6603ed1ec8335932f2de54
NavindaHewawickrama <hewawickraman@gmail.com>
2025-01-03 01:06:59 +0530

    bird image

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,10 +1,16 @@
 import 'package:flame/game.dart';
 import 'package:flappy_bird/components/background.dart';
+import 'package:flappy_bird/components/ground.dart';
+import 'package:flappy_bird/components/bird.dart';
 
 class FlappyBirdGame extends FlameGame{
-
+  late Bird bird;
   @override
   Future<void> onLoad() async {
-    add(Background());
+    addAll([
+      Background(),
+      Ground(),
+      bird = Bird(),
+    ]);
   }
 }
